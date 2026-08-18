74479f75fb25c6cfd2b3dc383c93dc263948d2e7
Wting0106 <Wting3653@gmail.com>
2026-08-18 23:14:29 +0800

    update again

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,7 +18,7 @@ module.exports = {
                     children: [
                         '/3DGS/',
                         '/3DGS/TEST0-Implicit-Neural-Representation',
-                        '/3DGS/3D Gaussian Splatting.md'
+                        '/3DGS/3D_Gaussian_Splatting'
                     ]
                 }
             ]
